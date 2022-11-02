5468004fe891148effd278a4193f331974af93a8
Nikita Cunskis <nikita.cunskis@gmail.com>
2022-11-02 03:35:59 +0200

    video added, design improvements, text added

--- resources/js/Pages/Join.vue
+++ resources/js/Pages/Join.vue
@@ -1,9 +1,29 @@
 <script setup>
 import FrontLayout from '@/Layouts/FrontLayout.vue';
 
+const props = defineProps({
+    title: {
+        type: String,
+        default: ""
+    },
+});
 </script>
 <template>
     <FrontLayout>
-        Join page
+        <video autoplay muted loop>
+            <source src="img/pexels-kindel-media-6774055.mp4" type="video/mp4">
+            <img src="img/strudel_solinsh.jpg" />
+        </video>
+
+        <p>Lai pievienotos velo skaitīšanas komandai nav obligāti jāiestājās biedrībā "Pilsētā Cilvekiem". Datu vākšana ir vairāku aktīvistu no vairākām apkaimēm un vairākām organizācijām brīvprātīgs ieguldījums Rīgas pilsētā. Katrs, kam ir brīvs laiks un var aprīkot sevi ar lapiņu un pildspalvu, vai mobīlo telefonu tiek aicināts velo skaitītāju komandā.</p>
+        <p>Galvenie nosacījumi:</p>
+        <ul>
+            <li> - mēs skaitam 15. datumam tuvākajā piektdienā 8:00 - 9:00</li>
+            <li> - vēlams skaitīt regulāri. Ja punktā neizdodas atrast skaitītāju, tad punkts netiks saskaitīts un datos veidosies nepatīkami robi.</li>
+            <li> - iekšējai komunikācijai, jūs tiksiet pievienots WhatsApp čatiņam</li>
+            <li> - pirmās prioritātes punkti = TILTI un PĀRVADI!</li>
+            <li> - nevari vairs skaitīt savu ierasto punktu? Nav problēma - vari paņemt citu brīvu! Nav laika? Neliela problēma - meklēsim, kas tevi aizvietos.</li>
+        </ul>
+        <p>Lai tiktu komandā, rakstat uz e-pastu info@pilsetacilvekiem.lv vai rakstat/zvanat pa tiešo Andrejam 29140848.</p>
     </FrontLayout>
 </template>
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -4,6 +4,12 @@ import FrontLayout from '@/Layouts/FrontLayout.vue';
 </script>
 <template>
     <FrontLayout>
-        Welcome page
+        <video autoplay muted loop>
+            <source src="img/pexels-cottonbro-5465259.mp4" type="video/mp4">
+            <img src="img/events.jpeg" />
+        </video>
+        <p>Velo skaitīšana ir nozīmīga aktivitāte, kura nepieciešama lai izpētīt, kā pilsētvides izmaiņas ietekmē velo satiksmi. Ir pašsaprotami, ka katrs cilvēks, kas izvēlās braukt ar velosipēdu - ir cilvēks, kas nebrauc ar automobīli, tādejādi, samazina sastrēgumus un palielina pilsētas kopējo drošību. </p>
+        <p>Šādas velo skaitīšanas tiek rīkotas uz regulāra pamata kopš 2018. gada, katru mēnesi piektdienā, kas vistuvāk atrodas 15. datumam no 8:00, līdz 9:00. Regularitāte nodrošināja "Pilsēta Cilvēkiem" un visiem, kas pieprasīja statistikas datus veikt regularus pētījumus un informēt Rīgas Domi un sabiedrību, par veiksmīgiem un neveiksmīgiem risinājumiem pilsētvidē Satiksmes jomā. Uzņēmumi var uzzināt aktualu informāciju par velosatiksmi blakus saviem veikaliem. Attīstītāji var prognozēt cik velonovietnes būs nepieciešams izvietot jaunajos projektos.</p>
+        <p>Līdz 2022. gadam šie dati nebija plaši izplatīti un tie tikai apkopoti vai publiskoti tikai pēc pieprasījuma. 2022. gadā, šie dati tiek atklāti sabiedrībai un tiek izstrādāts instruments, kas palīdz analizēt datus un sakatavot atskaites pēc interesējošiem parametriem.</p>
     </FrontLayout>
 </template>
