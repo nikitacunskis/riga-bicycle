9c5e487d70c1d21edf321167ebccd9f8ed01c81a
Nikita Cunskis <nikita.cunskis@gmail.com>
2022-11-02 04:57:00 +0200

    RAW data support

---
+++ app/Http/Controllers/RawpageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+use App\Models\Event;
+use App\Models\Place;
+use App\Models\Report;
+use App\Models\Weather;
+use App\Http\Requests\ReportRequest;
+
+class RawpageController extends Controller
+{
+    public function index(Request $request)
+    {
+        $reports = Report::all()->toArray();
+        foreach($reports as $key => $report)
+        {
+            $reports[$key]['place'] = Place::where('id', $report['place_id'])->pluck('location')[0];
+            $reports[$key]['event'] = Event::where('id', $report['event_id'])->pluck('date')[0];
+            $reports[$key]['weather'] = Event::where('id', $report['event_id'])->pluck('weather')[0];
+        }
+
+        return Inertia::render('Raw', ['reports' => $reports]);
+    }
+}
