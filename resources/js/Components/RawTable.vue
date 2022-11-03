ae2364cc019d04bad433651e654e7c38dcf3cf37
Nikita Cunskis <85262150+nikitacunskis@users.noreply.github.com>
2022-11-03 21:01:01 +0200

    Design improvements for selector page. Method checkboxes changed to radio

--- resources/js/Components/RawTable.vue
+++ resources/js/Components/RawTable.vue
@@ -26,7 +26,7 @@ function showAllWeather()
 </script>
 <template>
     <div class="overflow-x-auto relative">
-            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <table class="w-full text-xs text-left text-gray-500 dark:text-gray-400">
                 <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="py-3 px-6">
