6d607d44e56d4b8efd5c8adc21e6d896f126c919
NIKITA CUNSKIS <nikitacunskis@getvuelo.com>
2025-11-23 22:03:31 +0200

    Footer

--- resources/js/Components/SiteFooter.vue
+++ resources/js/Components/SiteFooter.vue
@@ -2,26 +2,26 @@
     <footer class="text-black py-8 px-4 sm:px-6 lg:px-8">
         <div class="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
             <div>
-                <h3 class="font-semibold">About</h3>
+                <h3 class="font-semibold">Par projektu</h3>
                 <p class="text-sm mt-2">
-                    Bicycle counting initiative supporting safer cycling in Riga.
+                    Pilsēta cilvēkiem veloskaitīšanas iniciatīva, kas atbalsta drošu un patīkamu pilsētvidi Rīgā.
                 </p>
             </div>
             <div>
-                <h3 class="font-semibold">Links</h3>
+                <h3 class="font-semibold">Saites</h3>
                 <ul class="mt-2 space-y-1 text-sm">
-                    <li><a href="#" class="hover:underline">Main Organization</a></li>
-                    <li><a href="#" class="hover:underline">Contact</a></li>
+                    <li><a href="https://pilsetacilvekiem.lv/" class="hover:underline">Pilsēta cilvēkiem</a></li>
+                    <li><a href="https://cunskis.lv/" class="hover:underline">Izstrātājs Cunskis.lv</a></li>
                 </ul>
             </div>
             <div>
-                <h3 class="font-semibold">Social</h3>
+                <h3 class="font-semibold">Sociālie tīkli</h3>
                 <ul class="mt-2 space-y-1 text-sm">
-                    <li><a href="#" class="hover:underline">Twitter</a></li>
-                    <li><a href="#" class="hover:underline">Facebook</a></li>
+                    <li><a href="https://x.com/PilsCilvekiem" class="hover:underline">X.com (Twitter)</a></li>
+                    <li><a href="https://www.facebook.com/PilsetaCilvekiem/" class="hover:underline">Facebook</a></li>
                 </ul>
             </div>
         </div>
-        <p class="text-center text-xs mt-6">© 2025 Riga Bicycle Count</p>
+        <p class="text-center text-xs mt-6">© 2025 Rīga, Pilsēta cilvēkiem veloskaitīšanas projekts</p>
     </footer>
 </template>
