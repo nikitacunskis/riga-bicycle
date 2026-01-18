61f49ca64000b4545d001fef6b67425bf03ddf37
Nikita Cunskis <85262150+nikitacunskis@users.noreply.github.com>
2026-01-18 23:38:01 +0200

    Login fix for HTTPS -> HTTP Proxy

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -10,8 +10,11 @@ window._ = _;
 import axios from 'axios';
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = {
-    'X-Requested-With': 'XMLHttpRequest'
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+const token = document.head.querySelector('meta[name="csrf-token"]');
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 }
 
 /**
